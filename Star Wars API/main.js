caf205d2ba42571593d56761463e4edc184fa6db
Christian Clarke <10542447@uvu.edu>
2020-10-27 03:09:03 -0600

    Star Wars API Base

--- Star Wars API/main.js
+++ Star Wars API/main.js
@@ -1,13 +1,19 @@
 import { films } from './data/films.js'
 import { people } from './data/people.js'
 
-// console.log(people.length)
+const main = document.querySelector('main')
 
-// console.log(films[0])
+for (let step = 0; step < 7; step++) {
+    let figure = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = 'https://starwars-visualguide.com/assets/img/films/2.jpg'
+    
+    let figCaption = document.createElement('figcaption')
+    figCaption.textContent = films[step].title
 
+    figure.appendChild(figImg)
+    figure.appendChild(figCaption)
 
+    main.appendChild(figure)
 
-films.forEach(film => {
-let newParagraph = document.body.appendChild(document.createElement('p'))
-newParagraph.textContent = film.title
-})
+}
