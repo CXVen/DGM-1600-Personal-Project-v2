997d6e66c226c6e39898795ce83d9b2f1a0c60c6
Christian Clarke <10542447@uvu.edu>
2020-12-17 18:19:40 -0700

    Fixed SW Films and hopefully resized pokemon image

--- StarWarsAPI/main.js
+++ StarWarsAPI/main.js
@@ -1,19 +1,21 @@
 import { films } from '../data/films.js'
+import { getLastNumber } from '../utils/index.js'
 
 const main = document.querySelector('main')
+
+
 
 for (let step = 0; step < 7; step++) {
     let figure = document.createElement('figure')
     let figImg = document.createElement('img')
-    figImg.src =   `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg`
-    
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg` 
     let figCaption = document.createElement('figcaption')
-    figCaption.textContent = films[step].title
+    
+    const foundFilm = films.find(film => parseInt(getLastNumber(film.url)) === (step + 1))
+    figCaption.textContent = foundFilm.title
 
     figure.appendChild(figImg)
     figure.appendChild(figCaption)
 
     main.appendChild(figure)
-
-}
-
+  }
